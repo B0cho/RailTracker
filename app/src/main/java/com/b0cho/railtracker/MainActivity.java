bc3ef6db51fd323c0ad752e650b86a0b0a0b2de6
B0cho <obiekt44@gmail.com>
2019-10-16 21:08:02 +0200

    Adding basic map view functionality

--- app/src/main/java/com/b0cho/railtracker/MainActivity.java
+++ app/src/main/java/com/b0cho/railtracker/MainActivity.java
@@ -1,12 +1,21 @@
 package com.b0cho.railtracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
 
 // MAIN ACTIVITY
 public class MainActivity extends AppCompatActivity {
-
+    private MapView mapView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +24,40 @@ public class MainActivity extends AppCompatActivity {
         // setting toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // mapview init
+        mapViewInit();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mapView.onResume();
+    }
+
+    @Override
+    public  void onPause() {
+        super.onPause();
+
+        mapView.onPause();
+    }
+
+    private void mapViewInit() {
+        // loading osmdroid configuration
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        
+        // setting map
+        mapView = findViewById(R.id.mapView);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.NEVER);
+        mapView.setMultiTouchControls(true);
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(7.5);
+        GeoPoint startingPoint = new GeoPoint(51.46, 19.27);
+        mapController.setCenter(startingPoint);
 
     }
 }
